223d6de3265b04c9df4783a1b79d632fa531834d
Egor Masterskikh <masterskikh.egor@gmail.com>
2024-09-29 00:24:13 +0200

    убрать папку out/ из индексации git и переименовать главный файл

--- md2html.sh
+++ md2html.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pandoc "ЦОС_ответы_на_вопросы_к_зачёту.md" --mathjax -V lang=ru -s -o "out/ЦОС_ответы_на_вопросы_к_зачёту.html"
+pandoc "main.md" --mathjax -V lang=ru -s -o "out/DSP-answers-to-exam-questions.html"
